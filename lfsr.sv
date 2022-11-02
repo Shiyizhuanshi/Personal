ed792c9bcc684868a12d21427f57fd38e0d06c68
Shiyizhuanshi <100481494+Shiyizhuanshi@users.noreply.github.com>
2022-11-02 19:54:20 +0000

    Update lfsr.sv

--- lfsr.sv
+++ lfsr.sv
@@ -1,21 +1,20 @@
-module sreg4 (
+module lfsr (
     input logic clk,
-    // input logic en,
+    input logic en,
     input logic rst,
     output [4:1] data_out
 );
 
-logic [4:1] sreg;
+    logic [4:1] sreg;
 
-always_ff @(posedge clk, posedge rst)
-    if (rst)
-        sreg <= 4'b1;
-    // else if (en)
-    else
-        sreg <= {sreg[3:1], sreg[4] ^ sreg[3]};
-    // else 
-    //     sreg <= sreg;
+    always_ff @(posedge clk, posedge rst)
+        if (rst)
+            sreg <= 4'b1;
+        else if (en)
+            sreg <= {sreg[3:1], sreg[4] ^ sreg[3]};
+        else 
+            sreg <= sreg;
 
-assign data_out = sreg[4];
+    assign data_out = sreg;
 
 endmodule
